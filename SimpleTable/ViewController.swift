d53e85a1e21097286304268881a9c0e2dfbb5877
Joseph <stndent123@yahoo.com.tw>
2020-08-31 01:20:49 +0800

    again

--- SimpleTable/ViewController.swift
+++ SimpleTable/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource,UITableViewDelegate {
+    var restaurantNames = ["Cafe Deadend","Homei","Teakha","Cafe Loisl","Petite Oyster","For Kee Restaurant","Po's Atelier","Bourke Street Bakery","Haigh's Chocolate","Palomino Espresso","Upstate","Traif","Graham Avenue Meats And Deli","Waffle & Wolf","Five Leaves","Cafe Lore","Confessional","Barrafina","Donostia","Royal Oak","CASK Pub and Kitchen"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 回傳區塊中的列數
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "datacell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        //設定CELL...
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: "restaurant")
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override var  prefersStatusBarHidden: Bool {
+      return true
+     //用來隱藏頂端的狀態列(時間日期)
+        
+  }
 }
 
